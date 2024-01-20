4045ecd6cd52c914c5005a0c35410dcc91c37bdb
fito422480 <adolfo.andres.ayala@gmail.com>
2024-01-20 19:16:15 -0300

    Subiendo el proyecto api de consulta de rucs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
-const rucsRoutes = require("./routes/rucs");
+const rucsRoutes = require("./src/routes/rucs");
 
 const app = express();
 const port = process.env.PORT || 9000;
